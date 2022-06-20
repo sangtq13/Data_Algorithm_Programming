2a42f966651466f14b8c1fbfc5d123feade3a226
Trần Quang Sang <61467361+sangtq13@users.noreply.github.com>
2022-06-20 15:06:27 +0700

    Update valid_parentheses.py

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -2,6 +2,8 @@ class Solution(object):
     def __init__(self):
         self.name = 'S->13'
 
+    # Time complexity: O(n)
+    # Space complexity: O(n)
     def validParentheses(self, s):
         p = {'[': ']', '{': '}', '(': ')'}
         stack = []
@@ -12,8 +14,16 @@ class Solution(object):
                 if not stack or p[stack.pop()] != m:
                     return False
 
-        return True if not stack else False
-
+        return len(stack) == 0
+    
+    # Using replace method:
+    # Time complexity: O(n^2)
+    # Space complexity: O(1)
+    def validParentheses(self, s):
+        for i in range(len(s)):
+            if len(s) != 0:
+                s = s.replace('()','').replace('{}','').replace('[]','')
+        return len(s) == 0      
 
 s = Solution()
 p = '(){([])}'
