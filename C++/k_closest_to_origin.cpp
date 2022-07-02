4282a0bc06c85424ec229f393ea2c8254ba3d252
Trần Quang Sang <61467361+sangtq13@users.noreply.github.com>
2022-07-02 16:46:28 +0700

    Update k_closest_to_origin.cpp

--- C++/k_closest_to_origin.cpp
+++ C++/k_closest_to_origin.cpp
@@ -42,4 +42,48 @@ public:
         }
         return ret;
     }
+    
+    int distance(vector<int> p) {
+        return p[0] * p[0] + p[1] * p[1];
+    }
+    
+    int partition(vector<vector<int>>& points, int left, int right) {
+        int mid = (left + right) / 2;
+        int index = left;
+        swap(points[mid], points[left]);
+        for (int i = left+1; i < right+1; ++i) {
+            if (distance(points[i]) <= distance(points[left])) {
+                index++;
+                swap(points[i], points[index]);
+            }
+        }
+        if (index < right + 1) {
+            swap(points[index], points[left]);
+        }
+        
+        return index;
+    }
+    // Using partition method
+    // Time complexity: O(n)
+    // Space complexity: O(k)
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        int l = points.size();
+        int left = 0;
+        int right = l-1;
+        int pivot = 0;
+        vector<vector<int>> ret;
+        while (pivot != k && left <= right) {
+            pivot = partition(points, left, right);
+            if (pivot < k) {
+                left = pivot + 1;
+            }
+            else if (pivot > k) {
+                right = pivot - 1;
+            }
+        }
+        for (int i = 0; i < k ; ++i) {
+            ret.push_back(points[i]);
+        }
+        return ret;
+    }
 };
