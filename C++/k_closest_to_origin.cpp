e294d00195e6c1015a0b87b28fef29e67713af45
Trần Quang Sang <61467361+sangtq13@users.noreply.github.com>
2022-07-02 15:00:36 +0700

    Update k_closest_to_origin.cpp

--- C++/k_closest_to_origin.cpp
+++ C++/k_closest_to_origin.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     typedef pair<int, int> pp;
-    // Using priority method
+    // Using priority method using min heap
     // Time complexity: O(n)
     // Space complexity: O(n)
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
@@ -10,6 +10,30 @@ public:
         for (int i = 0; i < points.size(); ++i) {
             int distance = points[i][0] * points[i][0] + points[i][1] * points[i][1];
             pq.push(make_pair(distance, i));
+        }
+        for (int i = 0; i < k; ++i) {
+            auto top = pq.top();
+            ret.push_back(points[top.second]);
+            pq.pop();
+        }
+        return ret;
+    }
+    
+    // Using priority method using max heap
+    // Time complexity: O(n)
+    // Space complexity: O(k)
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        vector<vector<int>> ret;
+        priority_queue<pp> pq;
+        for (int i = 0; i < points.size(); ++i) {
+            int distance = points[i][0] * points[i][0] + points[i][1] * points[i][1];
+            if(i < k) {
+                pq.push(make_pair(distance, i));
+            }
+            else if (distance < pq.top().first){
+                pq.pop();
+                pq.push(make_pair(distance, i));
+            }
         }
         for (int i = 0; i < k; ++i) {
             auto top = pq.top();
