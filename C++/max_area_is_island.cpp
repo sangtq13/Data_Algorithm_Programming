4d2472a08bc766af3a71500b53425353f1a7f486
Trần Quang Sang <61467361+sangtq13@users.noreply.github.com>
2022-07-02 10:23:29 +0700

    Update max_area_is_island.cpp

--- C++/max_area_is_island.cpp
+++ C++/max_area_is_island.cpp
@@ -12,7 +12,9 @@ public:
             }
         }
     }
-        
+    // Recursive method:
+    // Time complexity: O(n)
+    // Space complexity: O(n) for using stack of recursive method
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int rows = grid.size();
         int cols = grid[0].size();
@@ -24,6 +26,43 @@ public:
                     this->maxAreaHelper(grid, i, j, rows, cols, currentLen);
                     maxLen = max(maxLen, currentLen);
                     currentLen = 0;
+                }
+            }
+        }
+        return maxLen;
+    }
+    
+    // Iterative method using DFS
+    // Time complexity: O(n)
+    // Space complexity: O(n)
+    int maxAreaOfIslandIterative(vector<vector<int>>& grid) {
+        vector<vector<int>> directions {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        int maxLen = 0;
+        int rows = grid.size();
+        int cols = grid[0].size();
+        
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (grid[i][j] == 1) {
+                    int current = 0;
+                    stack<vector<int>> s;
+                    s.push({i, j});
+                    while (!s.empty()) {
+                        vector<int> p = s.top();
+                        s.pop();
+                        if (grid[p[0]][p[1]] == 0) continue;
+                        current++;
+                        grid[p[0]][p[1]] = 0;
+                        for (auto& d : directions) {
+                            int nx = p[0] + d[0];
+                            int ny = p[1] + d[1];
+                            if (0 <= nx && nx < rows && 0 <= ny && ny < cols && grid[nx][ny] == 1) {
+                                s.push({nx, ny});
+                            }
+                        }
+                    }
+                    if (current == 5) std::cout << i << j << std::endl;
+                    maxLen = max(current, maxLen);
                 }
             }
         }
