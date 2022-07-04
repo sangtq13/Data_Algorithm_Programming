8f791c0ebd5920670e7c1b48091e645a1387fa2f
Trần Quang Sang <61467361+sangtq13@users.noreply.github.com>
2022-07-04 22:53:08 +0700

    Create anagram_strings.cpp

---
+++ C++/anagram_strings.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool compare(int a[], int b[], int n) {
+        for (int i = 0; i < n; ++i) {
+            if (a[i] != b[i]) return false;
+        }
+        
+        return true;
+    }
+    
+    vector<int> findAnagrams(string s, string p) {
+        int ks[26] {0};
+        int kp[26] {0};
+        vector<int> ret;
+        int ls = s.size();
+        int lp = p.size();
+        if (ls < lp) return {};
+        
+        for (int i = 0; i < lp; ++i) {
+            ks[s[i] - 'a'] += 1;
+            kp[p[i] - 'a'] += 1;
+        }
+        
+        if (compare(ks, kp, 26)) ret.push_back(0);
+        for (int i = 1; i < ls - lp + 1; ++i) {
+            ks[s[i-1] - 'a'] -= 1;
+            ks[s[i+lp-1] - 'a'] += 1;
+            if (compare(ks, kp, 26)) {
+                ret.push_back(i);
+            }
+        }
+        
+        return ret;
+    }
+};
