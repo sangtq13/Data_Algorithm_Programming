2a7e3152937eeb4e4bdeef97b47f6979b6ee072d
Trần Quang Sang <61467361+sangtq13@users.noreply.github.com>
2022-07-05 09:39:40 +0700

    Create rectangle_overlap.js

---
+++ Javascript/rectangle_overlap.js
@@ -0,0 +1,4 @@
+# Time complexity: O(1)
+var isRectangleOverlap = function(rec1, rec2) {
+    return Math.max(rec1[0], rec2[0]) < Math.min(rec1[2], rec2[2]) && Math.max(rec1[1], rec2[1]) < Math.min(rec1[3], rec2[3])
+};
