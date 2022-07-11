5260d2dbaeb24b5e3bc6b17fca612b62089f13cf
Trần Quang Sang <61467361+sangtq13@users.noreply.github.com>
2022-07-11 10:40:46 +0700

    Update not_cheap_string.cpp

--- Codeforces/not_cheap_string.cpp
+++ Codeforces/not_cheap_string.cpp
@@ -1,7 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Using count method
+// Time complexity: O(n)
+// Space complexity: O(k)
 string Solution(string s, int num) {
+    int count[26] = {0};
+    int sum = 0;
+    for (auto &c : s) {
+        count[c - 'a'] += 1;
+        sum += (c - 'a' + 1);
+    }
+
+    int i = 25;
+    while (sum > num) {
+        while (i >= 0) {
+            if (count[i] == 0) {
+                i--;
+            }
+            else {
+                break;
+            }
+        }
+        sum -= (i+1);
+        count[i] -= 1;
+    }
+
+    string result = "";
+    for (auto &c : s) {
+        if (count[c - 'a'] > 0) {
+            result += c;
+            count [c - 'a'] -= 1;
+        }
+    }
+
+    return result;
+}
+
+// Using priority and hash map method
+// Time complexity: O(n)
+// Space complexity: O(n)
+string Solution1(string s, int num) {
     string result = "";
     priority_queue<char> pq;
     unordered_map<char, int> m;
