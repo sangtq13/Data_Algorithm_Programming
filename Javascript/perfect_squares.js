6ee33d58242f0ffa44d7fb6b4d8e196fd61a6aae
Trần Quang Sang <61467361+sangtq13@users.noreply.github.com>
2022-07-12 16:55:32 +0700

    Create perfect_squares.js

---
+++ Javascript/perfect_squares.js
@@ -0,0 +1,21 @@
+// Time complexity: O(n)
+// Space complexity: O(n)
+var numSquares = function(n) {
+    var i = 0;
+    squares = []
+    while (i * i <= n) {
+        squares.push(i * i)
+        i++
+    }
+    
+    min_sums = new Array(n+1).fill(n)
+    min_sums[0] = 0
+    for (var i = 0; i <= n; ++i) {
+        for (let s of squares) {
+            if (i + s <= n) {
+                min_sums[i + s] = Math.min(min_sums[i + s], min_sums[i] + 1)
+            }
+        }
+    }
+    return min_sums[min_sums.length-1]
+};
