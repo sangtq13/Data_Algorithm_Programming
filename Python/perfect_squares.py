46140741bddb4782a89fdd0376e6d5fd7e9fa9ab
Trần Quang Sang <61467361+sangtq13@users.noreply.github.com>
2022-07-12 16:56:09 +0700

    Create perfect_squares.py

---
+++ Python/perfect_squares.py
@@ -0,0 +1,22 @@
+class Solution():
+    def __init__(self):
+        self.name = 'S->13'
+
+    # Time complexity: O(n)
+    # Space complexity: O(n)
+    def SumOfSquares(self, n):
+        i = 1
+        squares = []
+        while i * i <= n:
+            squares.append(i*i)
+            i += 1
+
+        min_sums = [n] *  (n+1)
+        min_sums[0] = 0
+        for i in range(0, n+1):
+            for s in squares:
+                if i + s < n+1:
+                    min_sums[i+s] = min(min_sums[i+s], min_sums[i] + 1)
+        return min_sums[-1]
+n = 4
+print(Solution().SumOfSquares(n))
