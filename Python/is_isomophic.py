6290d558f7c44c7ec67ef02bec44ef6949a1f15a
Trần Quang Sang <61467361+sangtq13@users.noreply.github.com>
2022-07-26 15:06:41 +0700

    Update is_isomophic.py

--- Python/is_isomophic.py
+++ Python/is_isomophic.py
@@ -4,7 +4,7 @@ class Solution():
 
     # Time complexity: O(n)
     # Space complexity: O(n)
-    def IsIsomorphic(self, s, t):
+    def IsIsomorphic1(self, s, t):
         if len(s) != len(t):
             return False
 
@@ -23,7 +23,23 @@ class Solution():
                     return False
         return True
 
+    def convert(self, s):
+        d = {}
+        result = []
+        for i in range(len(s)):
+            if s[i] not in d:
+                d[s[i]] = i
+                result.append(i)
+            else:
+                result.append(d[s[i]])
+        return result
+    # Time complexity: O(n)
+    # Space complexity: O(n)
+    def IsIsomorphic2(self, s, t):
+        return self.convert(s) == self.convert(t)
 s = 'abc'
 t = 'def'
-print(Solution().IsIsomorphic(s, t))
-print(Solution().IsIsomorphic('aaaaa', 'bbbba'))
+print(Solution().IsIsomorphic1(s, t))
+print(Solution().IsIsomorphic1('aaaaa', 'bbbba'))
+print(Solution().IsIsomorphic2(s, t))
+print(Solution().IsIsomorphic2("bbbaaaba", "aaabbbba"))
