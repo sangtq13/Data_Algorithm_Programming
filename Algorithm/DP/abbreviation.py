af8bfd78e5f27e22f8c51a900aeb6e5d2375d3e2
Trần Quang Sang <61467361+sangtq13@users.noreply.github.com>
2022-10-06 00:10:11 +0700

    Update abbreviation.py

--- Algorithm/DP/abbreviation.py
+++ Algorithm/DP/abbreviation.py
@@ -2,44 +2,43 @@ class Solution:
     def __init__(self):
         self.name = 'S->13'
 
+    # Time complexity: O(m*n)
+    # Space complexity: O(m*n)
     def Abbreviation(self, a, b):
         if len(b) > len(a):
             return False
         da = {}
-        temp = {}
+        upper_num = 0
         for i in range(len(a)):
+            da[i] = upper_num
             if a[i].isupper():
-                if a[i] not in temp:
-                    da[i] = 0
-                    temp[a[i]] = 0
-                else:
-                    temp[a[i]] += 1
-                    da[i] = temp[a[i]]
-        temp.clear()
-        db = {}
-        for i in range(len(b)):
-            if b[i] not in temp:
-                db[i] = 0
-                temp[b[i]] = 0
-            else:
-                temp[b[i]] += 1
-                db[i] = temp[b[i]]
+                upper_num += 1
         dp = [[False] * len(a) for _ in range(len(b))]
         if a[0].upper() == b[0]:
             dp[0][0] = True
-        for i in range(len(b)):
-            for j in range(i, len(a)):
-                if i == 0 and j == 0:
-                    continue
-                if a[j].islower():
-                    dp[i][j] = dp[i][j-1] | (a[j].upper() == b[i])
+        for i in range(1, len(a)):
+            if a[i].islower():
+                if a[i].upper() == b[0] and da[i] == 0:
+                    dp[0][i] = True
                 else:
-                    if a[j] == b[i] and da[j] <= db[i]:
-                        dp[i][j] = True
-        print(da)
-        print(db)
-        print(dp)
+                    dp[0][i] = dp[0][i-1]
+            else:
+                if a[i] == b[0] and da[i] == 0:
+                    dp[0][i] = True
+        for i in range(1, len(b)):
+            for j in range(i, len(a)):
+                if a[j].islower():
+                    if a[j].upper() == b[i]:
+                        dp[i][j] = dp[i][j-1] | dp[i-1][j-1]
+                    else:
+                        dp[i][j] = dp[i][j-1]
+                else:
+                    if a[j] != b[i]:
+                        dp[i][j] = False
+                    else:
+                        dp[i][j] = dp[i-1][j-1]
+                        
         return dp[-1][-1]
-a = 'AbCdE'
-b = 'AFE'
+a = 'daBcde'
+b = 'ABC'
 print(Solution().Abbreviation(a,b))
