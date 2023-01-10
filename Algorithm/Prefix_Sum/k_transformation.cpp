89d2b5a3a1a14dabdeefd168f742d7de4b5a3a91
Trần Quang Sang <61467361+sangtq13@users.noreply.github.com>
2023-01-10 12:01:49 +0700

    Update k_transformation.cpp

--- Algorithm/Prefix_Sum/k_transformation.cpp
+++ Algorithm/Prefix_Sum/k_transformation.cpp
@@ -1,12 +1,25 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+
 class Solution {
   public:
+    // Time complexity: O(m*n*k)
+    // Space complexity: O(m*n*k)
     vector<int> k_Transformation(int n, int m, int k, vector<vector<int> > grid, int q, vector<vector<int> > queries) {
         // calculate prefix sum array arr[i][j][l]
         // This is total numbers mod k equal to l 
         // in subgird with [0, 0] coordinates top left 
         // and [i, j] coordinates top right corner
         vector<int> ans;
-        int pre[n][m][k] = {0};
+        int pre[n][m][k];
         for (int i = 0; i < n; ++i) {
             pre[i][0][0] = 0;
         }
@@ -31,19 +44,14 @@ class Solution {
         
         for (int i = 1; i < n; ++i) {
             for (int j = 1; j < m; ++j) {
-                pre[i][j][0] = pre[i][j-1][0] + pre[i-1][j][0] - pre[i-1][j-1][0] + (grid[i][j] % k == 0);
-            }
-        }
-        
-        for (int i = 1; i < n; ++i) {
-            for (int j = 1; j < m; ++j) {
-                for (int l = 1; l < k; ++l) {
+                for (int l = 0; l < k; ++l) {
                     pre[i][j][l] = pre[i-1][j][l] + pre[i][j-1][l] - pre[i-1][j-1][l] + (grid[i][j] % k == l);
                 }
             }   
         }
         
         for (auto& qr : queries) {
+            int count = 0;
             int t = qr[0];
             int val = qr[1];
             int lx = qr[2];
@@ -52,10 +60,53 @@ class Solution {
             int ry = qr[5];
             int l = val - t%k;
             if (l < 0) l += k;
-            int count = pre[rx][ry][l] - pre[lx][ry][l] - pre[rx][ly][l] + pre[lx][ly][l];
+            if (lx == 0 && ly == 0) {
+                count = pre[rx][ry][l];
+            }
+            else if (lx == 0) {
+                count = pre[rx][ry][l] - pre[rx][ly-1][l];
+            }
+            else if (ly == 0) {
+                count = pre[rx][ry][l] - pre[lx-1][ry][l];
+            }
+            else {
+                count = pre[rx][ry][l] - pre[lx-1][ry][l] - pre[rx][ly-1][l] + pre[lx-1][ly-1][l];
+            }
             ans.push_back(count);
         }
 
         return ans;
     }
 };
+
+// { Driver Code Starts.
+
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n, m, k;
+        cin >> n >> m >> k;
+
+        vector<vector<int> > grid(n, vector<int> (m, 0));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                cin >> grid[i][j];
+
+        int q;
+        cin >> q;
+        vector<vector<int> > queries(q, vector<int> (6, 0));
+        for (int i = 0; i < q; i++)
+            for (int j = 0; j < 6; j++)
+                cin >> queries[i][j];
+
+
+        Solution ob;
+        vector<int> ans = ob.k_Transformation(n, m, k, grid, q, queries);
+        for (auto it: ans)
+            cout << it << " ";
+        cout << "\n";
+    }
+    return 0;
+}
