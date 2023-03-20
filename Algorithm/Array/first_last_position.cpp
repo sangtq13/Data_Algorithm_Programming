5f5e989a3d461f1a6e4e3881446432f961ecf9ce
Trần Quang Sang <61467361+sangtq13@users.noreply.github.com>
2023-03-20 15:05:32 +0700

    Update first_last_position.cpp

--- Algorithm/Array/first_last_position.cpp
+++ Algorithm/Array/first_last_position.cpp
@@ -31,11 +31,49 @@ public:
     }
   } 
 
+  // 
   vector<int> searchRange(vector<int>& nums, int target) {
     int left_most = -1;
     int right_most = -1;
     int n = nums.size();
     this->binarySearch(nums, 0, n-1, target, left_most, right_most);
+    return {left_most, right_most};
+  }
+  
+  int iterativeSearch(vector<int>& nums, int low, int high, int target, bool left_most = false) 
+  {
+    while (low <= high) {
+      int mid = (low + high) / 2;
+      if (nums[mid] == target) {
+        if (left_most) {
+          if (mid == 0 || nums[mid-1] < nums[mid]) {
+            return mid;
+          }
+          high = mid - 1;
+        }
+        else {
+          if (mid == nums.size()-1 || nums[mid] < nums[mid+1]) {
+            return mid;
+          }
+          low = mid + 1;
+        }
+      }
+      else if (nums[mid] > target) {
+        high = mid -1;
+      }
+      else {
+        low = mid + 1;
+      }
+    }
+    return -1;
+  } 
+  // Iterative method
+  // Time complexity: O(logn)
+  // Space complexity: O(1)
+  vector<int> searchRange(vector<int>& nums, int target) {
+    int n = nums.size();
+    int left_most = iterativeSearch(nums, 0, n-1, target, true);
+    int right_most = iterativeSearch(nums, 0, n-1, target, false);
     return {left_most, right_most};
   }
 };
