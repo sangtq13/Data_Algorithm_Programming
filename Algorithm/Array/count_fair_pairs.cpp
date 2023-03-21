6404e74ee21edc107409d124fff48a0f2d70d04c
Trần Quang Sang <61467361+sangtq13@users.noreply.github.com>
2023-03-21 11:25:20 +0700

    Create count_fair_pairs.cpp

---
+++ Algorithm/Array/count_fair_pairs.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    
+    // Time complexity: O(nlogn)
+    // Space complexity: O(1)
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        sort(nums.begin(), nums.end());
+        int left = 0;
+        int right = nums.size() - 1;
+        long long count = 0;
+        while (left < right) {
+            long long total = (long long)nums[left] + nums[right];
+            if (total < lower) {
+                left++;
+            }
+            else if (total > upper) {
+                right--;
+            }
+            else {
+                count++;
+                cout << left << right << endl;
+                int low = left + 1;
+                int high = right - 1;
+                while (low <= high) {
+                    int mid = (low + high) / 2;
+                    if ((long long)nums[mid] + nums[right] <= upper) {
+                        low = mid + 1;
+                    }
+                    else {
+                        high = mid - 1;
+                    }
+                }
+                count += (low - left - 1);
+                low = left + 1;
+                high = right - 1;
+                while (low <= high) {
+                    int mid = (low + high) / 2;
+                    if ((long long)nums[mid] + nums[left] >= lower) {
+                        high = mid - 1;
+                    }
+                    else {
+                        low = mid + 1;
+                    }
+                }
+                count += (right - high - 1);
+                cout << count << endl;
+                left++;
+                right--;
+            }
+        }
+        return count;
+    }
+};
