691a76bca7a476cdce48f0bfa715a24fb36ed087
Trần Quang Sang <61467361+sangtq13@users.noreply.github.com>
2023-04-03 10:52:08 +0700

    Update combination_sum_2.cpp

--- Algorithm/Backtracking/combination_sum_2.cpp
+++ Algorithm/Backtracking/combination_sum_2.cpp
@@ -42,3 +42,34 @@ public:
         return ans;
     }
 };
+
+// Other solution with sorting and backtracking
+class Solution {
+public:
+    void backtracking(vector<int>& candidates, vector<vector<int>>& ans, 
+            vector<int>& cur, int start, int& remain) {
+        if (remain == 0) ans.push_back(cur);
+        if (remain <= 0 || start == candidates.size()) return;
+        int pre = -1;
+        for (int i = start; i < candidates.size(); ++i) {
+            if (candidates[i] == pre) continue;
+            cur.push_back(candidates[i]);
+            remain -= candidates[i];
+            backtracking(candidates, ans, cur, i+1, remain);
+            cur.pop_back();
+            remain += candidates[i];
+            pre = candidates[i];
+        }
+    }
+
+    // Time complexity: O(2^n)
+    // Space complexity: O(n)
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> cur;
+        sort(candidates.begin(), candidates.end());
+        backtracking(candidates, ans, cur, 0, target);
+
+        return ans;
+    }
+};
