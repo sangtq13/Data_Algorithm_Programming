83aa2cbc53bdfc12756a65f9a84a785218395b9a
Trần Quang Sang <61467361+sangtq13@users.noreply.github.com>
2023-04-05 11:09:41 +0700

    Create rotate_image.cpp

---
+++ Algorithm/Array/rotate_image.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    // Time complexity: O(n)
+    // Space complexity: O(1)
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        for (int i = 0; i <= (n-1)/2; ++i) {
+            for (int j = i; j < n-1-i; ++j) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = tmp;
+            }
+        } 
+    }
+};
