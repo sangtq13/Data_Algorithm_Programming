6fb6f7d1611fa6522eca0405967c5bf04231defc
Trần Quang Sang <61467361+sangtq13@users.noreply.github.com>
2024-01-05 22:26:59 +0700

    Update rotate_array.cpp

--- Algorithm/Array/rotate_array.cpp
+++ Algorithm/Array/rotate_array.cpp
@@ -38,3 +38,30 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    void swap(int& a, int &b) {
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+    // Time complexity: O(n)
+    // Space complexity: O(1)
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+
+        for (int i = 0; i < (n-k)/2; ++i) {
+            swap(nums[i], nums[n-k-1-i]);
+        }
+
+        for (int i = 0; i < k/2; ++i) {
+            swap(nums[n-k+i], nums[n-1-i]);
+        }
+
+        for (int i = 0; i < n/2; ++i) {
+            swap(nums[i], nums[n-1-i]);
+        }
+    }
+};
