86c76b6aa37ac5f1c190b947fd0f1721d81e0a1b
Trần Quang Sang <61467361+sangtq13@users.noreply.github.com>
2024-01-05 17:53:32 +0700

    Create rotate_array.cpp

---
+++ Algorithm/Array/rotate_array.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int gcd(int a, int b) 
+    { 
+        if (b == 0) 
+            return a; 
+        return gcd(b, a % b); 
+    } 
+
+    // Function to return LCM of two numbers  
+    int lcm(int a, int b) 
+    { 
+        return (a / gcd(a, b)) * b; 
+    }
+
+    void rotate(vector<int>& nums, int k, int count, int start) {
+        int c = 0;
+        int n = nums.size();
+        int pre = nums[start];
+        while (c < count) {
+            int new_index = (start + k) % n;
+            int tmp = nums[new_index];
+            nums[new_index] = pre;
+            pre = tmp;
+            start = new_index;
+            c++;
+        }
+    }
+
+    // Time complexity: O(n)
+    // Space complexity: O(1)
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        if (k == 0) return;
+        int l = lcm(n, k);
+        for (int i = 0; i < n / (l / k); ++i) {
+            rotate(nums, k, l / k, i);
+        }
+    }
+};
